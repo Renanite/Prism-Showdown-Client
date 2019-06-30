dd2ce1cdb834324dc03b23f5fa1fa612fc94a836
ADinnerofSpaghetti <peterswanton191@bellsouth.net>
2019-06-29 20:21:57 -0400

    crystal_clear

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 var Config = Config || {};
 
-/* version */ Config.version = "0.11.2 (bf9cd93f)";
+/* version */ Config.version = "0.11.2 (c295dcc7)";
 
 Config.bannedHosts = ['cool.jit.su', 'pokeball-nixonserver.rhcloud.com'];
 
